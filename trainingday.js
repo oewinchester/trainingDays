4a759be2c5f9024013207b5b6642e857f266ce43
Ömer Taha Öztürk <68539653+oewinchester@users.noreply.github.com>
2023-11-05 21:25:44 +0300

    Create trainingday.js

---
+++ trainingday.js
@@ -0,0 +1,44 @@
+// The scope of `random` is too loose 
+const name = 'Nala';
+
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+const getTrainingDays = (event) => {
+let days;
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
+  }
+
+  return days;
+};
+
+// The scope of `name` is too tight 
+const logEvent = (event) => {
+
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+
+logEvent(event); 
+logTime(days);
